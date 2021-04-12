f1dd9f60081a40bae3527cdc276c6ce62fa31583
Rismaalia2404 <80316932+Rismaalia2404@users.noreply.github.com>
2021-04-13 00:21:41 +0900

    Add files via upload

---
+++ MINGGU9/application/models/Hello_model.php
@@ -0,0 +1,13 @@
+<?php
+class Hello_model extends CI_Model{
+    //public $txt = 'Hello World!';
+    
+    public $alert = 'Hello Wolrd dari CI Model';
+    
+    public $mahasiswa = [
+			["nama"=>"Risma Raudlatul Aulia","nim"=>"E31190596","gol"=>"A"],
+			["nama"=>"Elysha Fitriana Dewi ","nim"=>"E31190416","gol"=>"A"],
+			["nama"=>"Prisca Dewi Anggita","nim"=>"E31190286","gol"=>"A"] 
+			["nama"=>"Adya Zalfa Zahirah","nim"=>"E31190957","gol"=>"A"] 
+		];
+}
